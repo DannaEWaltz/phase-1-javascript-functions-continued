5d491173e058b32b61c58bbad21494597982a2d0
+ <+dannaewaltz@gmail.com>
2022-06-28 11:06:05 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    function innerFunction(adj = "special") {
+        let result = wrapAdjective("*");
+        let emphatic = result("a hard worker");
+        return `You are ${flair} ${adj} ${flair}!`;
+    }
+}
+wrapAdjective();
